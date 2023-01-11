298dee17f97024ef591b35e36d48895aaf90230a
jessicaquaye <jquaye@g.harvard.edu>
2023-01-11 11:18:18 -0500

    forced alignment with cum of clips per category

--- forced_alignment.py
+++ forced_alignment.py
@@ -4,18 +4,18 @@ import shutil
 import string
 from pathlib import Path
 
-def populate_t_and_nt_audios(df, clips_dir, target_clips, non_target_clips, KEYWORD, NUM_OF_CLIPS_PER_CATEGORY):
+def populate_t_and_nt_audios(df, clips_dir, target_clips, non_target_clips, keyword, num_of_clips_per_category):
     #extract target and non-target
-    files_w_key = df[ df['sentence'].str.contains(KEYWORD).fillna(False) ]
-    files_wout_key = df[ ~df['sentence'].str.contains(KEYWORD).fillna(False) ]
+    files_w_key = df[ df['sentence'].str.contains(keyword).fillna(False) ]
+    files_wout_key = df[ ~df['sentence'].str.contains(keyword).fillna(False) ]
 
     #select target and non-target that will be used for training
-    target = list(files_w_key['path'])[:NUM_OF_CLIPS_PER_CATEGORY]
-    non_target = list(files_wout_key['path'])[:NUM_OF_CLIPS_PER_CATEGORY]
+    target = list(files_w_key['path'])[:num_of_clips_per_category]
+    non_target = list(files_wout_key['path'])[:num_of_clips_per_category]
 
     print("target clips size: ", len(target))
     print("non target clips size: ", len(non_target))
-
+    
     #copy audio clips into respective folders
     print("iterating through target files")
     for fname in target:
@@ -27,7 +27,7 @@ def populate_t_and_nt_audios(df, clips_dir, target_clips, non_target_clips, KEYW
             shutil.copy2(clips_dir / fname, non_target_clips / fname)
     return [(target_clips, target), (non_target_clips, non_target)]
 
-def create_word_labs(df, KEYWORD, data_and_aligns, clips_of_interest_list, clips_dir):
+def create_word_labs(df, keyword, data_and_aligns, clips_of_interest_list, clips_dir):
     written = 0
 
     all_words_in_src = set()
@@ -41,16 +41,16 @@ def create_word_labs(df, KEYWORD, data_and_aligns, clips_of_interest_list, clips
         lab_name = f"{basename}.lab"
 
         #fake that each wavfile is a separate speaker
-        word_labs = Path(KEYWORD + "/word_labs")
+        word_labs = Path(keyword + "/word_labs")
         os.makedirs(word_labs / basename)
 
         #write transcript
         lab_file = word_labs / basename / lab_name
-        with open( lab_file, "w", encoding="utf8") as fh:
+        with open(lab_file, "w", encoding="utf8") as fh:
             fh.write(transcript)
 
         if os.path.exists(clips_dir / fname):
-            shutil.copy2(clips_dir / fname, word_labs / basename) #copy audio to world lab dir
+            # shutil.copy2(clips_dir / fname, word_labs / basename) #copy audio to world lab dir
             shutil.copy2(clips_dir / fname, data_and_aligns / "data") #copy audio to data and aligns
             shutil.copy2(lab_file, data_and_aligns / "data") #copy lab to data and aligns
             written += 1
@@ -72,12 +72,12 @@ def create_lexicons(lexicon_path, all_words):
     return
 
 #go from csv to lexicons
-def extract_from_tsv(tsv_path, clips_dir, target_clips, non_target_clips, data_and_aligns, KEYWORD, NUM_OF_CLIPS_PER_CATEGORY):
+def extract_from_tsv(tsv_path, clips_dir, target_clips, non_target_clips, data_and_aligns, keyword, num_of_clips_per_category):
     df = pd.read_csv(tsv_path, sep='\t') #load data into pandas df
 
     #separate words into target and non-target
     print("populate t and non_t clips")
-    result = populate_t_and_nt_audios(df, clips_dir, target_clips, non_target_clips, KEYWORD, NUM_OF_CLIPS_PER_CATEGORY)
+    result = populate_t_and_nt_audios(df, clips_dir, target_clips, non_target_clips, keyword, num_of_clips_per_category)
 
     #CREATE WORD LABS 
     print("creating word labs")
@@ -85,45 +85,75 @@ def extract_from_tsv(tsv_path, clips_dir, target_clips, non_target_clips, data_a
 
     _, target_list = result[0]
     print("target list from pop t: ", len(target_list))
-    all_target_words = create_word_labs(df, KEYWORD, data_and_aligns, target_list, clips_dir)
+    all_target_words = create_word_labs(df, keyword, data_and_aligns, target_list, clips_dir)
 
+
+    print("starting non-target processing")
     _, non_target_list = result[1]
     print("non target list from pop nt: ", len(non_target_list))
-    all_ntarget_words = create_word_labs(df, KEYWORD, data_and_aligns, non_target_list, clips_dir)
+    all_ntarget_words = create_word_labs(df, keyword, data_and_aligns, non_target_list, clips_dir)
 
     #CREATE LEXICONS
     print("creating lexicons")
     all_words = all_target_words | all_ntarget_words
     print("Num words", len(all_words))
-    lexicon_path = Path(KEYWORD + "/lexicon.txt")
+    lexicon_path = Path(keyword + "/lexicon.txt")
     create_lexicons(lexicon_path, all_words)
 
     return
 
-def generate_mfas(KEYWORD, NUM_OF_CLIPS_PER_CATEGORY):
+def generate_mfas(keyword, num_of_clips_per_category):
     abs_path = os.path.abspath(__file__)
     base_dir = os.path.dirname(abs_path) + "/"
     clips_dir = Path(base_dir + "cv-corpus-wav/clips/")
 
     # initialize folders
-    target_clips = Path(KEYWORD + '/target/')
-    os.makedirs(target_clips)
-    non_target_clips = Path(KEYWORD +'/non_target/')
+    non_target_clips = Path(keyword +'/non_target/')
     os.makedirs(non_target_clips)
 
-    data_and_aligns = Path(KEYWORD + "/data_and_alignments/")
+    data_and_aligns = Path(keyword + "/data_and_alignments/")
     os.makedirs(data_and_aligns / "data")
     os.makedirs(data_and_aligns / "alignments")
 
+    print("extracting training clips")
     train_tsv = 'cv-corpus-wav/train.tsv'
-    extract_from_tsv(train_tsv, clips_dir, target_clips, non_target_clips, data_and_aligns, KEYWORD, NUM_OF_CLIPS_PER_CATEGORY)
+    train_target_clips = Path(keyword + '/target/train/')
+    os.makedirs(train_target_clips)
+    extract_from_tsv(keyword=keyword,
+                    tsv_path=train_tsv,
+                    clips_dir=clips_dir,
+                    target_clips=train_target_clips,
+                    non_target_clips=non_target_clips,
+                    data_and_aligns=data_and_aligns,
+                    num_of_clips_per_category=num_of_clips_per_category)
 
+    print("extracting validation clips")
+    dev_tsv = 'cv-corpus-wav/dev.tsv'
+    dev_target_clips = Path(keyword + '/target/dev/')
+    os.makedirs(dev_target_clips)
+    extract_from_tsv(keyword=keyword,
+                    tsv_path=dev_tsv,
+                    clips_dir=clips_dir,
+                    target_clips=dev_target_clips,
+                    non_target_clips=non_target_clips,
+                    data_and_aligns=data_and_aligns,     
+                    num_of_clips_per_category=num_of_clips_per_category)
+
+    print("extracting testing clips")
     test_tsv = 'cv-corpus-wav/test.tsv'
-    extract_from_tsv(test_tsv, clips_dir, target_clips, non_target_clips, data_and_aligns, KEYWORD, NUM_OF_CLIPS_PER_CATEGORY)
+    test_target_clips = Path(keyword + '/target/test/')
+    os.makedirs(test_target_clips)
+    extract_from_tsv(keyword=keyword,
+                    tsv_path=test_tsv,
+                    clips_dir=clips_dir,
+                    target_clips=test_target_clips,
+                    non_target_clips=non_target_clips,
+                    data_and_aligns=data_and_aligns,
+                    num_of_clips_per_category=num_of_clips_per_category)
 
     #run mfa train command to produce alignments
     data_path = base_dir + str(data_and_aligns) + "/data/"
-    lexicon_path = base_dir + KEYWORD + "/lexicon.txt"
+    lexicon_path = base_dir + keyword + "/lexicon.txt"
     alignments_path = base_dir + str(data_and_aligns) + "/alignments/"
     mfa_train_command = "mfa train --clean " + data_path + " " + lexicon_path + " " + alignments_path
     os.system(mfa_train_command)
